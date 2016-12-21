937660e768427cd3a3b128b32966d9b939984e20
Daniel Kostro <kostro.d@gmail.com>
2016-12-21 11:03:36 +0100

    refactor: put device socket-io logic in a separate file

--- src/server/server.js
+++ src/server/server.js
@@ -1,94 +1,13 @@
 'use strict';
-const debug = require('debug')('main:sever socket-io');
 const express = require('express');
-var app = express();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+const app = express();
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+const ioDevices = require('./socketio/devices');
 
 server.listen(3000);
 
 app.use(express.static('src/public'));
-
-var DeviceFactory = require('../devices/DeviceFactory');
 // Emit welcome message on connection
-io.on('connection', function (socket) {
-    debug('connection');
-    // at startup of the connection, all the deviceList is sent,
-    // the ready event ensures the code init in the visualizer is served before the events
-
-    socket.on('ready', function () {
-        var deviceList = DeviceFactory.getDeviceList();
-        for (let key in deviceList) {
-            socket.emit('newDevice', {id: parseInt(key), type: deviceList[key].type, status: deviceList[key].status});
-        }
-    });
-
-    //handling server requests
-    socket.on('request', function (request, fn) {
-
-        if (request.type === 'static-method') {
-            debug('received static request from client:' + JSON.stringify(request));
-            debug('device constructor is ' + request.constructorName);
-            var constructor = require('../devices/' + request.constructorName + '/' + request.constructorName);
-            var method = request.method;
-            if (constructor[method] === undefined) {
-                fn({
-                    status: 'error',
-                    error: 'no device present corresponding to the request'
-                });
-            } else {
-                Promise.resolve(constructor[method].apply(null, request.args))
-                    .then((data) => ({status: 'success', data: data}))
-                    .then(fn)
-                    .catch((err) => fn({status: 'error', error: err.message}));
-            }
-
-        } else if (request.type === 'method') {
-            var device = DeviceFactory.getDevice(request.id);
-            debug('received request from client:' + JSON.stringify(request));
-            debug('device id is ' + request.id);
-            debug('device is:' + JSON.stringify(device));
-            if (device === undefined) {
-                fn({
-                    status: 'error',
-                    error: 'no device present corresponding to the request'
-                });
-            } else {
-                Promise.resolve(device[request.method].apply(device, request.args))
-                    .then((data) => ({status: 'success', data: data}))
-                    .then(fn)
-                    .catch((err) => fn({status: 'error', error: err.message}));
-            }
-        }
-
-
-    });
-
-    socket.on('disconnect', function () {
-        debug('stopping socket.io client');
-    });
-
-    socket.on('error', err => debug(`socket error: ${err.message}`));
-
-});
-
-
-// //Listeners
-DeviceFactory.on('newDevice', device => {
-    io.emit('newDevice', {
-        id: device.id,
-        type: device.type,
-        status: device.status
-    });
-});
-
-DeviceFactory.on('connect', id => {
-    debug('connect event server:' + id);
-    io.emit('deviceConnected', id);
-});
-
-DeviceFactory.on('disconnect', id => {
-    debug('disconnect event server:' + id);
-    io.emit('deviceDisconnected', id);
-});
+ioDevices(io);
 
---
+++ src/server/socketio/devices.js
@@ -0,0 +1,88 @@
+'use strict';
+const DeviceFactory = require('../../devices/DeviceFactory');
+const debug = require('debug')('socketio:devices');
+
+
+module.exports = function(io) {
+
+    io.on('connection', function (socket) {
+        debug('connection');
+        // at startup of the connection, all the deviceList is sent,
+        // the ready event ensures the code init in the visualizer is served before the events
+
+        socket.on('ready', function () {
+            var deviceList = DeviceFactory.getDeviceList();
+            for (let key in deviceList) {
+                socket.emit('newDevice', {id: parseInt(key), type: deviceList[key].type, status: deviceList[key].status});
+            }
+        });
+
+        //handling server requests
+        socket.on('request', function (request, fn) {
+
+            if (request.type === 'static-method') {
+                debug('received static request from client:' + JSON.stringify(request));
+                debug('device constructor is ' + request.constructorName);
+                var constructor = require('../devices/' + request.constructorName + '/' + request.constructorName);
+                var method = request.method;
+                if (constructor[method] === undefined) {
+                    fn({
+                        status: 'error',
+                        error: 'no device present corresponding to the request'
+                    });
+                } else {
+                    Promise.resolve(constructor[method].apply(null, request.args))
+                        .then((data) => ({status: 'success', data: data}))
+                        .then(fn)
+                        .catch((err) => fn({status: 'error', error: err.message}));
+                }
+
+            } else if (request.type === 'method') {
+                var device = DeviceFactory.getDevice(request.id);
+                debug('received request from client:' + JSON.stringify(request));
+                debug('device id is ' + request.id);
+                debug('device is:' + JSON.stringify(device));
+                if (device === undefined) {
+                    fn({
+                        status: 'error',
+                        error: 'no device present corresponding to the request'
+                    });
+                } else {
+                    Promise.resolve(device[request.method].apply(device, request.args))
+                        .then((data) => ({status: 'success', data: data}))
+                        .then(fn)
+                        .catch((err) => fn({status: 'error', error: err.message}));
+                }
+            }
+
+
+        });
+
+        socket.on('disconnect', function () {
+            debug('stopping socket.io client');
+        });
+
+        socket.on('error', err => debug(`socket error: ${err.message}`));
+
+    });
+
+    // //Listeners
+    DeviceFactory.on('newDevice', device => {
+        io.emit('newDevice', {
+            id: device.id,
+            type: device.type,
+            status: device.status
+        });
+    });
+
+    DeviceFactory.on('connect', id => {
+        debug('connect event server:' + id);
+        io.emit('deviceConnected', id);
+    });
+
+    DeviceFactory.on('disconnect', id => {
+        debug('disconnect event server:' + id);
+        io.emit('deviceDisconnected', id);
+    });
+
+};
