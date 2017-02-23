1ca90c1e91e46132d9d0760380abf88f544759de
lpatiny <gb252243>
2017-02-23 23:51:37 +0100

    Remove console.log

--- examples/deviceFactory.js
+++ examples/deviceFactory.js
@@ -5,8 +5,13 @@ const DeviceFactory = require('../src/devices/DeviceFactory');
 DeviceFactory.on('newDevice', device => {
     console.log('New device');
     device.getParsedCompactLog().then(data => {
-        console.log('q res', data);
+        console.log(data);
     });
+
+    device.getMultiLog().then(data => {
+        console.log(data);
+    })
+
 });
 
 DeviceFactory.on('error', err => console.log);
