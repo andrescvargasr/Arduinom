af7a0ad50f3954de69002af1f4cf97c867f0e82f
stropitek <kostro.d@gmail.com>
2017-02-26 15:01:25 +0100

    add usage example of IncPoll

---
+++ examples/incPoll.js
@@ -0,0 +1,29 @@
+'use strict';
+
+const IncPoll = require('./../lib/utilities/IncPoll');
+const arr = Array.from({length:12}).map((val, idx) => {
+    return idx;
+});
+
+const incPoll = new IncPoll({
+    interval: 300,
+    chunk: 5,
+    task: function (inc) {
+        return new Promise(function(resolve) {
+            console.log('poll');
+            setTimeout(() => {
+                resolve(arr.slice(inc, inc + 5));
+            }, 3000);
+        });
+    },
+    start: 3
+});
+
+incPoll.on('data', data => console.log('data', data));
+incPoll.on('progress', progress => {console.log(progress) });
+
+incPoll.start();
+
+setTimeout(() => {
+    //incPoll.stop();
+},2000);
