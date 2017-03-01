37980f6609d739d573099a985893fee06e5f30c2
stropitek <kostro.d@gmail.com>
2017-03-01 21:45:21 +0100

    Launch synchro option in server script

--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const server = require('http').Server(app);
+const argv = require('minimist')(process.argv.slice(2));
 const io = require('socket.io')(server);
 const ioDevices = require('./socketio/devices');
 
@@ -10,4 +11,11 @@ server.listen(3000);
 app.use(express.static('src/public'));
 // Emit welcome message on connection
 ioDevices(io);
+
+if(argv['sync-db']) {
+    // Launch db sync
+    require('../db/sync');
+}
+
+
 
