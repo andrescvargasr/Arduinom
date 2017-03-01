5749a74848547b406a0983a81c510a13a3b028bc
stropitek <kostro.d@gmail.com>
2017-03-01 21:39:15 +0100

    sync: make sure id is not max uint32

--- src/db/sync.js
+++ src/db/sync.js
@@ -6,6 +6,7 @@ const deviceFactory = require('../devices/DeviceFactory');
 const connection = require('./connection');
 const debug = require('debug')('db:sync');
 const IncPoll = require('../utilities/IncPoll');
+const MAX_UINT32 = Math.pow(2,32) - 1;
 
 deviceFactory.on('newDevice', async function (device) {
     // Get start
@@ -18,7 +19,10 @@ deviceFactory.on('newDevice', async function (device) {
             debug(`get multiLog and save to db (inc: ${inc})`);
             // Get last entries from device
             const parsed = await device.getParsedMultiLog(inc);
-            await connection.saveEntries(deviceId, parsed);
+
+            // Make sure we don't save entries with abnormal id
+            const toSave = parsed.filter(p => parsed.id !== MAX_UINT32);
+            await connection.saveEntries(deviceId, toSave);
             return parsed;
         },
         chunk: 10,
